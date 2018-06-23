c0764343056a52262ab2563d6b0eefdd6c92637f
Craig Spaeth <craigspaeth@gmail.com>
2018-06-23 14:21:17 -0400

    Airtable as a CMS prototype

--- apps/prototypes/lib/practices.js
+++ apps/prototypes/lib/practices.js
@@ -1,10 +1,17 @@
 import _ from 'lodash'
 import jsforce from 'jsforce'
 import cheerio from 'cheerio'
+import Airtable from 'airtable'
 
 const salesforce = new jsforce.Connection({
   proxyUrl: 'http://localhost:3000/sfproxy/'
 })
+
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY
+})
+const base = Airtable.base('appdiwobKWKmEb7tl')
 
 // WARNING: This would be very insecure code for production, as it'll expose
 // a login/password in plain sight. We'd wrap this code in our own authenticated
@@ -80,4 +87,77 @@ export const submitExercise = async (objName, data) => {
   const res = await salesforce.sobject(objName).create(data)
   console.log(res)
   return res
+}
+
+const find = (tableName, formula) => {
+  let records = []
+  return new Promise((resolve, reject) => {
+    base(tableName)
+      .select({
+        maxRecords: 100,
+        filterByFormula: formula
+      })
+      .eachPage(
+        (rs, fetchNextPage) => {
+          rs.forEach(r => records.push(r._rawJson.fields))
+          fetchNextPage()
+        },
+        err => {
+          err ? reject(err) : resolve(records)
+        }
+      )
+  })
+}
+
+export const findPracticeCategoriesForClient = async email => {
+  const client = (await find('Clients', `{Email} = '${email}'`))[0]
+  const categories = await find(
+    'Practice Categories',
+    `Clients = '${client.Name}'`
+  )
+  const categoriesWithPracticesAndBlocks = _.flatten(
+    await Promise.all(
+      categories.map(async category => {
+        const practices = await find(
+          'Practices',
+          `{Practice Categories} = '${category.Name}'`
+        )
+        const practicesWithBlocks = await Promise.all(
+          practices.map(async practice => {
+            const blocks = await find(
+              'Practice Blocks',
+              `Practice = '${practice.Name}'`
+            )
+            return { practice, blocks }
+          })
+        )
+        return { category, practices: practicesWithBlocks }
+      })
+    )
+  )
+  const cleaned = categoriesWithPracticesAndBlocks.map(
+    ({ category, practices }) => ({
+      title: category.Name,
+      practices: practices.map(({ practice, blocks }) => ({
+        thumbnail: practice.Thumbnails[0].thumbnails.large.url,
+        name: practice.Name,
+        href: `/practice?name=${practice.Name}`,
+        blocks: blocks.map(block => ({
+          type: block.Type,
+          ...{
+            Input: {
+              label: block['Input Label']
+            },
+            Audio: {
+              url: block['Audio URL']
+            },
+            Text: {
+              content: block['Text Content']
+            }
+          }[block.Type]
+        }))
+      }))
+    })
+  )
+  return cleaned
 }

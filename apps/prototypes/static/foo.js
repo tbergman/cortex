cfedfcc533bd985cb5ea25c9bfa6d0f078cf4ba0
Craig Spaeth <craigspaeth@gmail.com>
2018-07-18 10:06:12 -0400

    Clean out prototypes and spikes

--- apps/prototypes/static/foo.js
+++
@@ -1,7 +0,0 @@
-console.log('0')
-self.addEventListener('sync', function (event) {
-  console.log('1')
-  event.waitUntil(async () => {
-    console.log('Send to Airtable')
-  })
-})
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express')
-const playground = require('./apps/playground')
-const prototypes = require('./apps/prototypes')
 const home = require('./apps/home')
 
 const { PORT } = process.env
 const app = express()
 const port = PORT || 3000
 
-app.use(playground)
-app.use(prototypes)
 app.use(home)
 
 app.listen(port, () => console.log(`Listening on ${port}`))

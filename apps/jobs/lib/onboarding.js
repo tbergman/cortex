2d6a828ef158955c7931c55810dc74759c445f85
Craig Spaeth <craigspaeth@gmail.com>
2018-07-27 20:44:22 -0400

    Refactor notifications lib

--- apps/jobs/lib/onboarding.js
+++ apps/jobs/lib/onboarding.js
@@ -2,66 +2,6 @@
  * After a lead completes preboarding or class scheduling where we poll Airtable
  * to find `onboarded` Signup Stage and invite them to signup to the app.
  */
-import twilio from 'twilio'
-import bcrypt from 'bcrypt'
-import Airtable from 'airtable'
+import * as notifications from './notifications'
 
-const {
-  AIRTABLE_API_KEY,
-  AIRTABLE_BASE_ID,
-  SMS_ONBOARDING,
-  TWILIO_ACCOUNT_SID,
-  TWILIO_BCRYPT_SALT,
-  TWILIO_SERVICE_SID,
-  TWILIO_TOKEN
-} = process.env
-const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_TOKEN)
-
-const sendWelcomeSMS = async phoneNum => {
-  const identity = (await bcrypt.hash(phoneNum, TWILIO_BCRYPT_SALT)).replace(
-    /[\W_]+/g,
-    ''
-  )
-  await client.notify.services(TWILIO_SERVICE_SID).bindings.create({
-    identity,
-    bindingType: 'sms',
-    address: phoneNum
-  })
-  await client.notify.services(TWILIO_SERVICE_SID).notifications.create({
-    body: SMS_ONBOARDING,
-    identity
-  })
-}
-
-const findOnboardingLeads = async () => {
-  const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID)
-  const records = await new Promise((resolve, reject) => {
-    let recs = []
-    base('Leads')
-      .select({
-        maxRecords: 10,
-        filterByFormula: "{Signup Stage} = 'onboarding'"
-      })
-      .eachPage(
-        (records, fetchNextPage) => {
-          records.forEach(rec => recs.push(rec.fields))
-          fetchNextPage()
-        },
-        err => {
-          err ? reject(err) : resolve(recs)
-        }
-      )
-  })
-  return records
-}
-
-export default async () => {
-  const leads = await findOnboardingLeads()
-  await Promise.all(
-    leads.map(async lead => {
-      const phoneNum = '+1' + lead['Phone Number'].replace(/[\W_]+/g, '')
-      await sendWelcomeSMS(phoneNum)
-    })
-  )
-  console.log('📬 Welcome notifications sent!')
-}
+export default async () => notifications.sendToleads('onboarding')
---
+++ apps/jobs/test/notifications.test.js
@@ -0,0 +1,60 @@
+import { sendToleads } from '../lib/notifications'
+
+let twilioMsg, twilioBindingPhoneNum, mailgunMsg
+
+jest.mock('airtable', () => {
+  const base = jest.fn().mockReturnValue({
+    select: jest.fn().mockReturnValue({
+      eachPage: (pageCallback, done) => {
+        const record = {
+          fields: {
+            sms: 'Yo! World',
+            emailSubject: 'Hello World',
+            emailBody: 'Well hello there world, how are you today',
+            'Email Address': 'karen@horney.com',
+            'Phone Number': '(555)-123-4567'
+          }
+        }
+        pageCallback([record], jest.fn())
+        done()
+      }
+    })
+  })
+  return () => ({ base: () => base })
+})
+jest.mock('bcrypt', () => ({ hash: async () => '$abc_123^' }))
+jest.mock('twilio', () => () => ({
+  notify: {
+    services: () => ({
+      bindings: {
+        create: data => {
+          twilioBindingPhoneNum = data.address
+        }
+      },
+      notifications: {
+        create: data => {
+          twilioMsg = data
+        }
+      }
+    })
+  }
+}))
+jest.mock('mailgun-js', () => () => ({
+  messages: () => ({
+    send: data => (mailgunMsg = data)
+  })
+}))
+
+test(`sendToleads sends onboarding leads a welcome SMS`, async () => {
+  await sendToleads()
+  expect(twilioMsg).toMatchObject({ identity: 'abc123', body: 'Yo! World' })
+  expect(twilioBindingPhoneNum).toEqual('+15551234567')
+})
+
+test.only(`sendToleads sends onboarding leads a welcome email`, async () => {
+  await sendToleads()
+  expect(mailgunMsg.html).toMatch(
+    '<p>Well hello there world, how are you today</p>'
+  )
+  expect(mailgunMsg.to).toEqual('karen@horney.com')
+})

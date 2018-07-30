6ff1434f5ef3d7daecf179e1388d65ed08275db6
Craig Spaeth <craigspaeth@gmail.com>
2018-07-30 12:29:36 -0400

    Poll for imprint interview added

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,8 +1,13 @@
+/**
+ * GraphQL API and associated endpoints like an image resizer under the /api
+ * namespace powered by Apollo Server
+ */
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
 import resizer from './lib/resizer'
 import * as Lead from './models/lead'
 import * as ContentModule from './models/content-module'
+import * as Appointment from './models/appointment'
 
 const app = express()
 
@@ -13,13 +18,14 @@ app.get('/api/image', resizer)
 const typeDefs = gql`
 ${Lead.schema.types}
 ${ContentModule.schema.types}
+${Appointment.schema.types}
   type Mutation {
     ${Lead.schema.mutations}
-    ${ContentModule.schema.mutations}
   }
   type Query {
     ping: String
     ${ContentModule.schema.queries}
+    ${Lead.schema.queries}
   }
 `
 
@@ -36,6 +42,7 @@ const resolvers = {
   }
 }
 
+// Apply server to Express instance
 new ApolloServer({ typeDefs, resolvers }).applyMiddleware({ app, path: '/api' })
 
 export default app

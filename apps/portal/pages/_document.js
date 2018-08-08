d478354ee243e7ec9d6e34ab390429f1f8fc15ea
Craig Spaeth <craigspaeth@gmail.com>
2018-08-08 11:42:25 -0400

    Clean up styling for demo

--- apps/portal/pages/_document.js
+++ apps/portal/pages/_document.js
@@ -1,7 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import React from 'react'
 import * as nextMui from 'next-mui'
-import { colors, reset, margins, navHeight } from 'styles'
+import { colors, reset, margins, navHeight, type } from 'styles'
 import Nav from 'components/nav'
 
 export default class Doc extends Document {
@@ -22,14 +22,7 @@ export default class Doc extends Document {
           <title>Octave | Wellness and Therapy</title>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <style>{reset}</style>
-        </Head>
-        <body>
-          <div id='container'>
-            <Nav />
-            <Main />
-            <NextScript />
-          </div>
-          <style jsx>
+          <style>
             {`
               body {
                 background: ${colors.dustyRose};
@@ -39,8 +32,30 @@ export default class Doc extends Document {
                 margin: auto;
                 padding: ${navHeight + margins.m}px 0 100px 0;
               }
+              iframe {
+                height: calc(100vh - 200px);
+                width: 100%;
+              }
+              h1 {
+                ${type.estebanL}
+                margin-bottom: ${margins.m}px;
+              }
+              p {
+                ${type.apercuM}
+                margin: ${margins.m}px 0;
+              }
+              img {
+                margin-bottom: ${margins.m}px;
+              }
             `}
           </style>
+        </Head>
+        <body>
+          <div id='container'>
+            <Nav />
+            <Main />
+            <NextScript />
+          </div>
         </body>
       </html>
     )
